5db20bfff5cab5eff30f7343709e4debfa7b921d
Michael Lee <Stat@Michaels-MacBook-Pro.local>
2014-04-08 12:31:42 -0400

    Initial Commit

---
+++ NextUp/MMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MMAppDelegate.h
+//  NextUp
+//
+//  Created by Michael Lee on 4/8/14.
+//  Copyright (c) 2014 Michael Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NextUp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NextUp
+//
+//  Created by Michael Lee on 4/8/14.
+//  Copyright (c) 2014 Michael Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MMAppDelegate class]));
+    }
+}
